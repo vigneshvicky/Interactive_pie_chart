86f3c4e090dfc3758223150ad335adfb6b37463c
vigneshvicky <vickybees@gmail.com>
2017-07-11 19:28:12 +0530

    Clockwise and anticlockwise rotation done
    Clockwise and anticlockwise rotation done

--- js/BasicLogic.js
+++ js/BasicLogic.js
@@ -1,23 +1,16 @@
 ///<reference path="typings/index.d.ts" />
 var BasicLogic = (function () {
     function BasicLogic() {
-        this.chartMidX = 180;
-        this.chartMidY = 190;
-        this.radius = 150;
-        this.totalSlice = 5;
+        this.chartMidX = 100;
+        this.chartMidY = 100;
+        this.radius = 100;
+        //public totalSlice:number = 5;
+        /*public previousSlice:JQuery;
+        public currentSlice:Object;
+        public nextSlice:JQuery;
+        private currentSliceNum:number;*/
         this.minimumSlicePercentage = 5;
         this.numberReg = /\d/;
     }
-    BasicLogic.prototype.setCurrentSlice = function (_currentSlice) {
-        this.currentSlice = _currentSlice;
-        this.currentSliceNum = this.getNumber(_currentSlice["id"]);
-        var tempId = this.currentSliceNum ? ~-this.currentSliceNum : this.totalSlice - 1;
-        this.previousSlice = $("#sliceBorder" + tempId);
-        tempId = (this.currentSliceNum == this.totalSlice - 1) ? 0 : -~this.currentSliceNum;
-        this.nextSlice = $("#sliceBorder" + tempId);
-    };
-    BasicLogic.prototype.getNumber = function (_sliceName) {
-        return Number(_sliceName.match(this.numberReg)[0]);
-    };
     return BasicLogic;
 }());

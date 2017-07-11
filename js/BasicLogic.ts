94d85c42bc713d198d0e66e3d74736e933613355
vigneshvicky <vickybees@gmail.com>
2017-07-11 19:24:26 +0530

    Clockwise and anticlockwise rotation done
    Clockwise and anticlockwise rotation done

--- js/BasicLogic.ts
+++ js/BasicLogic.ts
@@ -5,25 +5,26 @@ class BasicLogic{
 	public chartMidX:number = 100;
 	public chartMidY:number = 100;
 	public radius:number = 100;
-	public totalSlice:number = 5;
+	//public totalSlice:number = 5;
 
-	public previousSlice:JQuery;
+	/*public previousSlice:JQuery;
 	public currentSlice:Object;
 	public nextSlice:JQuery;
-	private currentSliceNum:number;
+	private currentSliceNum:number;*/
 
 	public minimumSlicePercentage:number = 5;
 
 	private numberReg = /\d/;
-	public setCurrentSlice(_currentSlice:Object):void{
+	/*public setCurrentSlice(_currentSlice:Object):void{
 		this.currentSlice = _currentSlice;
+		$(_currentSlice).attr("");
 		this.currentSliceNum = this.getNumber(_currentSlice["id"]);
 		let tempId = this.currentSliceNum?~-this.currentSliceNum:this.totalSlice-1;
 		this.previousSlice = $("#sliceBorder"+tempId);
 		tempId = (this.currentSliceNum==this.totalSlice-1)?0:-~this.currentSliceNum;
 		this.nextSlice = $("#sliceBorder"+tempId);
-	}
-	public getNumber(_sliceName:string):number{
+	}*/
+	/*public getNumber(_sliceName:string):number{
 		return Number(_sliceName.match(this.numberReg)[0]);
-	}
+	}*/
 }

7fea33d6bf26b94ad36201dab1983c83b99c85f2
vigneshvicky <vickybees@gmail.com>
2017-07-11 19:23:11 +0530

    Clockwise and anticlockwise rotation done
    Clockwise and anticlockwise rotation done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 angular
-.module("chart",["ngTouch","localStorage"])        
+.module("chart",["localStorage"])        
 	.directive("pieChart",function(){
         return{
             restrict:"E",
@@ -25,9 +25,12 @@ angular
 					$scope.percent = [];
 					$scope.default_angles = [];
 					$scope.currentChartId = "";
+					$scope.defaultTextRadius = $scope.radius-25;
 					$scope.colorsData = $scope.colors.split(",");
+					$scope.pieSliceDatas = [];
 					//$scope.totalSlice = $scope.colorsData.length;
-
+					$scope.previousAngle = 0;
+					$scope.totalPIEAngle = 360;
 					$scope.doInitialSetup();
 					if($scope.percentage){
 						var tempArr = $scope.percentage.split(",");
@@ -47,13 +50,13 @@ angular
 					
 					$scope.setXPos;
 				},
-				template:"<div class=\"stagesize\">"+
+				template:"<div>"+
 							"<svg height=\"{{radius*2+border_thickness}}\" width=\"{{radius*2+border_thickness}}\">"+
                             	"<circle cx=\"{{radius+half_border_thickness}}\" cy=\"{{radius+half_border_thickness}}\" r=\"{{radius}}\" fill=\"#AABBCC\"/>"+
                             	"<g class=\"slice_line\" ng-repeat=\"myang in default_angles track by $index\" >"+
             						"<path d=\"{{generateSlice(myang,$index)}}\" stroke=\"#ffffff\" stroke-width=\"0\" fill=\"#{{colorsData[$index]}}\"></path>"+
             					"</g>"+
-	                            "<g  class=\"pie_slice\" ng-repeat=\"arr in colorsData track by $index\" custAttr=\"{{attrTestFunc($index)}}\" id=\"{{mychartid}}_stick_{{$index}}\" transform =\"{{getStyle($index)}}\" fill=\"none\" style=\"cursor:pointer;\" ng-touchmove=\"startMoveSlice($event)\" ng-mousedown=\"startMoveSlice($event)\">"+
+	                            "<g class=\"pie_slice\" ng-repeat=\"arr in colorsData track by $index\" custAttr=\"{{attrTestFunc($index)}}\" id=\"{{mychartid}}_stick_{{$index}}\" transform =\"{{getStyle($index)}}\" fill=\"none\" style=\"cursor:pointer;\" ng-mousedown=\"startMoveSlice($event)\">"+
 	                                "<rect x=\"0\" y=\"-5\" width=\"{{radius}}\" height=\"10\" style=\"fill:blue; fill-opacity:0;\"></rect>"+
 	                                "<line stroke=\"#FFFFFF\" stroke-width=\"2\" x1=\"0\" y1=\"0\" x2=\"{{radius}}\" y2=\"0\" style=\"stroke-opacity:1\"/>"+
 	                            "</g>"+								                         
@@ -69,6 +72,7 @@ angular
 			//$scope.curAngle = 0;
 			//mathLogics.basicLogics.totalSlice = $scope.totalSlice;
 			//mathLogics.basicLogics.chartMidY = $scope.radius;
+			var previousAngle = 0;
 			$scope.getdetails = function(){
 				console.log("caling");
 			}
@@ -96,14 +100,14 @@ angular
 				return val+":";
 			}
 			$scope.startMoveSlice = function(evt){
-				$scope.currentChartId = mathLogics.basicLogics.getNumber($($scope.directiveElement).attr("chartid"));
+				$scope.currentChartId = mathLogics.getNumber($($scope.directiveElement).attr("chartid"));
 				//$scope.currentChart = $($scope.directiveElement).attr("chartid");
 				mathLogics.setScope ($scope);
 				//console.log("Radius of current pie chart : "+$scope.radius);			
-				mathLogics.basicLogics.setCurrentSlice(evt.currentTarget);
+				mathLogics.setCurrentSlice(evt.currentTarget);
 
-				$document.on('mousemove touchMove', mMove_javascript);
-				$document.on('mouseup touchend', stopMove_javascript);
+				$document.on('mousemove', mMove_javascript);
+				$document.on('mouseup', stopMove_javascript);
 
 				//console.log($( ".pie_slice" ).index( $(evt.currentTarget) ) );
 			}
@@ -116,60 +120,139 @@ angular
 			}
 			function stopMove_javascript(){
 				//console.log("stop move"+$scope.radius);
-				$document.off('mousemove touchmove', mMove_javascript);
-				$document.off('mouseup touchend', stopMove_javascript);
+				$document.off('mousemove', mMove_javascript);
+				$document.off('mouseup', stopMove_javascript);
 			}
 			$scope.mMove = function(evt){
-				var curPos = mathLogics.getCurrentMousePosition(evt,$(mathLogics.basicLogics.currentSlice).parent());
+				var curPos = mathLogics.getCurrentMousePosition(evt,$(mathLogics.currentSlice).parent());
 				//$("#test_dot").css({left:evt.clientX+"px",top:evt.clientY+"px"});
-				currentIdNum = mathLogics.basicLogics.getNumber($(mathLogics.basicLogics.currentSlice).attr("id"));
+				currentIdNum = $($scope.directiveElement).find(".pie_slice").index(mathLogics.currentSlice); //mathLogics.getNumber($(mathLogics.currentSlice).attr("id"));
 				var curAngle = mathLogics.getAngle(curPos.posX,curPos.posY,($scope.radius+$scope.half_border_thickness),($scope.radius+$scope.half_border_thickness));
-				$(mathLogics.basicLogics.currentSlice).attr({'transform': "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+(curAngle-0)+",0,0)"});
+				$(mathLogics.currentSlice).attr({'transform': "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+(curAngle-0)+",0,0)"});
 				//$scope.theta = 'translate(101,101) rotate('+$scope.curAngle+',0,0)';
 				
 				//console.log($(mathLogics.basicLogics.currentSlice).attr('transform'));
+			
+			
+				previousIdNum = $($scope.directiveElement).find(".pie_slice").index(mathLogics.previousSlice);
+				
+				nextIdNum = $($scope.directiveElement).find(".pie_slice").index(mathLogics.nextSlice);
 
 
-			/*currentIdNum = mathLogics.basicLogics.getNumber($(mathLogics.basicLogics.currentSlice).attr("id"));
-			var curAngle = mathLogics.getAngle(curPos.posX,curPos.posY);
-			$(mathLogics.basicLogics.currentSlice).css({'transform': 'rotate('+curAngle+'deg)'});
-			
-			previousIdNum = mathLogics.basicLogics.getNumber($(mathLogics.basicLogics.previousSlice).attr("id"));
-			
-			nextIdNum = mathLogics.basicLogics.getNumber($(mathLogics.basicLogics.nextSlice).attr("id"));			
-			
-			previousElementPos = mathLogics.getPositionByAngle(mathLogics.getAngleByElement($(mathLogics.basicLogics.previousSlice)).deg);
-			
-			nextElementPos = mathLogics.getPositionByAngle(mathLogics.getAngleByElement($(mathLogics.basicLogics.nextSlice)).deg);
-			
-			if(previousAngle>350 && curAngle<10){
-				mathLogics.isRotateClockWise = true;
-			}else if(previousAngle<10 && curAngle>350){
-				mathLogics.isRotateClockWise = false;
-			}else{
-				mathLogics.isRotateClockWise = previousAngle<curAngle;				
+				var previousElementPos = mathLogics.getPositionByAngle(mathLogics.getAngleByElement($(mathLogics.previousSlice)).deg);
+				
+				var nextElementPos = mathLogics.getPositionByAngle(mathLogics.getAngleByElement($(mathLogics.nextSlice)).deg);
+				
+				if(previousAngle>350 && curAngle<10){
+					mathLogics.isRotateClockWise = true;
+				}else if(previousAngle<10 && curAngle>350){
+					mathLogics.isRotateClockWise = false;
+				}else{
+					mathLogics.isRotateClockWise = previousAngle<curAngle;				
+				}
+				previousAngle = curAngle;
+				var currentPosition = mathLogics.getPositionByAngle(curAngle);			
+				
+				var myValPrev = mathLogics.getAngleByElement($(mathLogics.previousSlice)).deg;
+				var myValNext = mathLogics.getAngleByElement($(mathLogics.nextSlice)).deg;
+							
+				var prev_curr_angle = mathLogics.getDifference(curAngle,myValPrev);
+				var curr_next_angle = mathLogics.getDifference(myValNext,curAngle);
+
+				var pos1 = mathLogics.getPositionByAnglesForLabel(curAngle,myValNext,$scope.defaultTextRadius);			
+				var pos2 = mathLogics.getPositionByAnglesForLabel(myValPrev,curAngle,$scope.defaultTextRadius);
+				
+				$scope.drawSlice(currentIdNum,currentPosition,nextElementPos,curr_next_angle);
+				$scope.drawSlice(previousIdNum,previousElementPos,currentPosition,prev_curr_angle);
+
+				if(mathLogics.getPercentage(curr_next_angle)<=5||mathLogics.getPercentage(prev_curr_angle)<=5){
+				if(mathLogics.isRotateClockWise){
+					$scope.checkClockwise_MinAngle(currentIdNum);
+				}else{
+					$scope.checkAnticlockWise_MinAngle(currentIdNum);
+				}
+			}	
+			$scope.pieSliceDatas[currentIdNum] = {x:pos1.left,y:pos1.top,angle:mathLogics.getPercentageByTwoElements($("#sliceBorder"+nextIdNum),$("#sliceBorder"+currentIdNum))};
+			$scope.pieSliceDatas[previousIdNum] = {x:pos2.left,y:pos2.top,angle:mathLogics.getPercentageByTwoElements($("#sliceBorder"+currentIdNum),$("#sliceBorder"+previousIdNum))};
+			//scope.$parent.$apply();
+
 			}
-			previousAngle = curAngle;
-			var currentPosition = mathLogics.getPositionByAngle(curAngle);			
-			
-			var myValPrev = mathLogics.getAngleByElement($(mathLogics.basicLogics.previousSlice)).deg;
-			var myValNext = mathLogics.getAngleByElement($(mathLogics.basicLogics.nextSlice)).deg;
-						
-			var prev_curr_angle = mathLogics.getDifference(curAngle,myValPrev);
-			var curr_next_angle = mathLogics.getDifference(myValNext,curAngle);		
-			
-			var pos1 = mathLogics.getPositionByAnglesForLabel(curAngle,myValNext,scope.$parent.defaultTextRadius);			
-			var pos2 = mathLogics.getPositionByAnglesForLabel(myValPrev,curAngle,scope.$parent.defaultTextRadius);
-			
-			
-			
-			drawSlice(currentIdNum,currentPosition,nextElementPos,curr_next_angle);
-			drawSlice(previousIdNum,previousElementPos,currentPosition,prev_curr_angle);*/
+			$scope.checkClockwise_MinAngle = function(currId){
+				var nextIdNum = currId == (~-$scope.totalSlice)?0:currId+1;
+				if(mathLogics.getPercentageByTwoElements($($scope.directiveElement).find(".pie_slice").eq(nextIdNum),$($scope.directiveElement).find(".pie_slice").eq(currId))<=5){
+					var currentAng = mathLogics.getAngleByElement($($scope.directiveElement).find(".pie_slice").eq(currId)).deg;
+					var nextAng = currentAng+18;
+					$($scope.directiveElement).find(".pie_slice").eq(nextIdNum).attr('transform', "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+nextAng+",0,0)");
+					currentElementPos = mathLogics.getPositionByAngle(currentAng);
+					
+					nextElementPos = mathLogics.getPositionByAngle(nextAng);
+					curr_next_angle = mathLogics.getDifference(nextAng,currentAng);
+					
+					$scope.drawSlice(currId,currentElementPos,nextElementPos,curr_next_angle);
+					var after_NextIdNum = nextIdNum == (~-$scope.totalSlice)?0:nextIdNum+1;
+					var after_NextAng;				
+					if(mathLogics.getPercentageByTwoElements($($scope.directiveElement).find(".pie_slice").eq(after_NextIdNum),$($scope.directiveElement).find(".pie_slice").eq(nextIdNum))<=5){
+						after_NextAng = nextAng+18;
+						$($scope.directiveElement).find(".pie_slice").eq(after_NextIdNum).attr('transform', "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+after_NextAng+",0,0)");
+					}else{
+						after_NextAng = mathLogics.getAngleByElement($($scope.directiveElement).find(".pie_slice").eq(after_NextIdNum)).deg;
+					}
+					var after_NextPos = mathLogics.getPositionByAngle(after_NextAng);
+					
+					var next_afterNext_angle_diff = mathLogics.getDifference(mathLogics.getAngleByElement($($scope.directiveElement).find(".pie_slice").eq(after_NextIdNum)).deg,nextAng);
+					
+					$scope.drawSlice(nextIdNum,nextElementPos,after_NextPos,next_afterNext_angle_diff);
+					
+					var nextAfterNext_text_pos = mathLogics.getPositionByAnglesForLabel(nextAng,after_NextAng,$scope.defaultTextRadius);
+					
+					$scope.pieSliceDatas[nextIdNum] = {x:nextAfterNext_text_pos.left,y:nextAfterNext_text_pos.top,angle:mathLogics.getPercentage(next_afterNext_angle_diff)};
+				
+					$scope.checkClockwise_MinAngle(nextIdNum);
+				}
 			}
-			
-			/*drawSlice = function(sliceId, fromPos, toPos, angleDiff){
-				$("#slice"+sliceId).attr("d","M"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY+" L"+fromPos.left+","+fromPos.top+" A"+mathLogics.basicLogics.radius+","+mathLogics.basicLogics.radius+",0,"+(angleDiff>180?1:0)+",1,"+toPos.left+","+toPos.top+" L"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY+" A0,0,0,0,0,"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY);
-			}*/	
+			$scope.checkAnticlockWise_MinAngle = function (currId){
+			//From previous to current slice drawing
+			var prevIdNum = currId?currId-1:~-$scope.totalSlice;	
+			if(mathLogics.getPercentageByTwoElements($($scope.directiveElement).find(".pie_slice").eq(currId),$($scope.directiveElement).find(".pie_slice").eq(prevIdNum))<=5){						
+				var currentAng = mathLogics.getAngleByElement($($scope.directiveElement).find(".pie_slice").eq(currId)).deg;
+				var prevAng = currentAng-18;
+				$($scope.directiveElement).find(".pie_slice").eq(prevIdNum).attr('transform', "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+prevAng+",0,0)");
+				
+				currentElementPos = mathLogics.getPositionByAngle(currentAng);
+				prevElementPos = mathLogics.getPositionByAngle(prevAng);
+				curr_prev_angle = mathLogics.getDifference(currentAng,prevAng);
+				
+				//currentElementPos = mathLogics.getPositionByAngle(currentAng);
+				var prevId = currId?currId-1:~-$scope.totalSlice;
+				$scope.drawSlice(prevId,prevElementPos,currentElementPos,curr_prev_angle);				
+				
+				var before_PrevIdNum = prevIdNum?prevIdNum-1:(~-$scope.totalSlice);
+				var before_PrevPos = mathLogics.getPositionByElement($($scope.directiveElement).find(".pie_slice").eq(before_PrevIdNum));
+				var prev_beforePrev_angle;
+				if(mathLogics.getPercentageByTwoElements($($scope.directiveElement).find(".pie_slice").eq(prevIdNum),$($scope.directiveElement).find(".pie_slice").eq(before_PrevIdNum))<=5){
+					prev_beforePrev_angle = prevAng-18;
+					$($scope.directiveElement).find(".pie_slice").eq(before_PrevIdNum).attr('transform', "translate("+($scope.radius+$scope.half_border_thickness)+","+($scope.radius+$scope.half_border_thickness)+") rotate("+prev_beforePrev_angle+",0,0)");
+				}else{
+					prev_beforePrev_angle = mathLogics.getAngleByElement($($scope.directiveElement).find(".pie_slice").eq(before_PrevIdNum)).deg;
+				}			
+				
+				var prev_beforePrev_angle_diff = mathLogics.getDifference(prevAng, prev_beforePrev_angle);
+				
+				prevId = prevIdNum?prevIdNum-1:~-$scope.totalSlice;
+				$scope.drawSlice(prevId,before_PrevPos,prevElementPos,prev_beforePrev_angle_diff);
+				
+				var prevBeforePrev_text_pos = mathLogics.getPositionByAnglesForLabel(prev_beforePrev_angle,prevAng,$scope.defaultTextRadius);
+				$scope.pieSliceDatas[prevId] = {x:prevBeforePrev_text_pos.left,y:prevBeforePrev_text_pos.top,angle:mathLogics.getPercentage(prev_beforePrev_angle_diff)};				
+				
+				$scope.checkAnticlockWise_MinAngle(prevIdNum);
+				
+			}
+		}
+			$scope.drawSlice = function(sliceId, fromPos, toPos, angleDiff){
+				//console.log(sliceId);
+				$($scope.directiveElement).find(".slice_line").eq(sliceId).find("path").attr("d","M"+$scope.chartMidX+","+$scope.chartMidY+" L"+fromPos.left+","+fromPos.top+" A"+$scope.radius+","+$scope.radius+",0,"+(angleDiff>180?1:0)+",1,"+toPos.left+","+toPos.top+" L"+$scope.chartMidX+","+$scope.chartMidY+" A0,0,0,0,0,"+$scope.chartMidX+","+$scope.chartMidY);
+				//$("#slice"+sliceId).attr("d","M"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY+" L"+fromPos.left+","+fromPos.top+" A"+mathLogics.basicLogics.radius+","+mathLogics.basicLogics.radius+",0,"+(angleDiff>180?1:0)+",1,"+toPos.left+","+toPos.top+" L"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY+" A0,0,0,0,0,"+mathLogics.basicLogics.chartMidX+","+mathLogics.basicLogics.chartMidY);
+			}	
 			$scope.addSlice = function(colorCode){
 				//$($scope.directiveElement).find("g").eq(0).css("display","none");
 				$scope.colorsData.push(colorCode);				
